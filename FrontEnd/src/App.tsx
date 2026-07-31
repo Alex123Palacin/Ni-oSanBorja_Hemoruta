5228b0e063f866ab50aee296a7399da1fa8e3266
Alex123Palacin <152417813+Alex123Palacin@users.noreply.github.com>
2026-07-31 18:00:29 -0500

    ññ

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Navigate, Routes, Route } from "react-router-dom";
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/ññññ' element={<ññññ />} />
+
+      </Routes>
+
+
+    </Router>
+
+    
+  )
+}
+
+export default App
